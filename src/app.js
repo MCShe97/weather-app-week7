9fbc208012d282599bd4f7e3050fda6609efd4c0
MCShe97 <83926794+MCShe97@users.noreply.github.com>
2021-07-26 06:39:41 -0600

    fixing errors attempt five

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ function formatDate(timestamp) {
 
 }
 
-function formatDay(timeStamp) {
+function formatDay(timestamp) {
     let date = new Date (timestamp * 1000);
     let day = date.getDay();
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
